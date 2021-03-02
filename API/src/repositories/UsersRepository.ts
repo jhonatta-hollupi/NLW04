660f475c9bc8867e08ca15dc577b3de8570b8add
Jhonatta Hollupi <jhonhollup@hotmail.com>
2021-03-02 11:54:24 -0300

    Projeto next level week rocketseat 4

---
+++ API/src/repositories/UsersRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "../models/User";
+
+@EntityRepository(User)
+class UsersRepostory extends Repository<User> {}
+
+export { UsersRepostory };
